4d255d67c9ce595650f77de509eea81f1f7dfc23
RAAM150 <raam150@hotmail.com>
2016-10-22 12:07:15 -0500

    Add files via upload

---
+++ Scoreboard/ObjectiveContainer.java
@@ -0,0 +1,13 @@
+package Scoreboard;
+
+import java.util.ArrayList;
+
+public class ObjectiveContainer
+{
+  public ArrayList<String> scores = new ArrayList<>();
+
+  public ObjectiveContainer(ArrayList<String> scores)
+  {
+    this.scores = scores;
+  }
+}
